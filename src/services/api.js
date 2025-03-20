f038bdfb3d3ca3dfd1c323a792a1dce427b5189c
Marcos Donaciano Teixeira Júnior <marcosdjunior@gmail.com>
2025-03-20 17:20:44 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'https://api-my-bank.vercel.app/',
+    baseURL: 'https://apiprojetongcash.vercel.app/',
     timeout: 10000,
     headers: { 'Content-Type': 'application/json' }
 });
