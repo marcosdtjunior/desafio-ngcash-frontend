f9181bc5904ee611de1f7493d9f712d18a0dba2d
Marcos Donaciano Teixeira Júnior <marcosdjunior@gmail.com>
2025-12-06 18:21:37 -0300

    Atualização do arquivo index.js principal

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './styles/index.css';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import MainRoutes from './routes';
+import './styles/index.css';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <BrowserRouter>
       <MainRoutes />
     </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
